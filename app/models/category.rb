f59d78daf93fd397d25a034103910c7820483ec3
Peter Kordel <pkordel@gmail.com>
2011-05-14 17:10:35 +0200

    category name for slug

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,14 @@
 class Category
   include Mongoid::Document
   field :name
+  field :slug
   key :name
   validates_presence_of :name
   validates_uniqueness_of :name
   has_many :events
+  before_save :generate_slug
+  protected
+  def generate_slug
+    self.slug = name
+  end
 end
